a7acf84784720148b134cec9be2c1be5f383542a
Sondra Boyle <sb359120@ohio.edu>
2021-12-06 23:24:19 -0500

    two distinct markers
    hiro and kanji markers

--- WaterApp.js
+++ WaterApp.js
@@ -1,7 +1,15 @@
 
-var sceneEl = document.querySelector('a-scene');
+var markerEl = document.querySelector('#kanji');
+var markerTwo = document.querySelector('#hiro');
 
 //create the text entity
 var entityEl = document.createElement('a-entity');
 entityEl.setAttribute('text', 'value:hello; align: center; height:4; width:4;');
-sceneEl.appendChild(entityEl);
+markerEl.appendChild(entityEl);
+
+var entityTwo = document.createElement('a-entity');
+entityTwo.setAttribute('text', 'value:goodbye; align: center; height:4; width:4;');
+markerTwo.appendChild(entityTwo);
+
+
+console.log(entityEl);
